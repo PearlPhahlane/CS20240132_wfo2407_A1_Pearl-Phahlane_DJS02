b93bff345fe9fc332244869ab5039cbb88f439a8
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-10-22 11:17:36 +0200

    use Math.floor to return rounded down value

--- scripts.js
+++ scripts.js
@@ -5,5 +5,6 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  result.innerText = Math.floor(dividend / divider); //P: round down value and assin the value directly to result.innerText
+
 });
