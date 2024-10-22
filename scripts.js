087e1e61c3fef050e1375b1fa7b4d42465cd9050
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-10-22 12:55:50 +0200

    refractor code by checking if inputs are empty, then assign new variable to convert the empty string to numbers, then conditional to check divison by 0

--- scripts.js
+++ scripts.js
@@ -6,6 +6,21 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  (dividend === "" || divider === "") ? result.innerText = `Division not performed. Both values are required in inputs. Try again.` : result.innerText = Math.floor(dividend / divider); //P: round down value and assin the value directly to result.innerText
+  //P: to check if inputs are empty 
+  if (dividend === "" || divider === "") {
+    result.innerText = `Division not performed. Both values are required in inputs. Try again.`;
+    return;
+  } 
 
+//P: to convert input values to numbers
+const dividendNumber = Number(dividend);
+const dividerNumber = Number(divider);
+
+//P: to check for division by 0
+if(dividerNumber === 0) {
+  result.innerText = `Division not performed. Invalid number provided. Try again.`;
+  console.error(`Division not performed. Invalid number provided. Try again.`);
+} else {
+  result.innerText = Math.floor(dividendNumber / dividerNumber);
+}
 });
