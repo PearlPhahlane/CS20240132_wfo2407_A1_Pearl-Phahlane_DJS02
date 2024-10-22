4be7c8e9737210057939310437c78327e7ec76f2
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-10-22 11:48:00 +0200

    add ternary operator to check if there are no entries added in the input fields, if no entries an error message will be displayed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
@@ -5,6 +6,6 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider); //P: round down value and assin the value directly to result.innerText
+  (dividend === "" || divider === "") ? result.innerText = `Division not performed. Both values are required in inputs. Try again.` : result.innerText = Math.floor(dividend / divider); //P: round down value and assin the value directly to result.innerText
 
 });
